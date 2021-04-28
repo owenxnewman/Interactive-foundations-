66ffdc6297e7b3b718f2f04b31d5482c4be9a867
owenxnewman <78100364+owenxnewman@users.noreply.github.com>
2021-04-28 11:30:43 -0700

    add updated files

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
 
-console.log('accordion demo');
+console.log('test');
 
 
 $(document).ready(function(){
-
-	//------------------------------------
-	// show hide each itme
-	//------------------------------------
-
-	$('.item').click(function(){
-		$(this).toggleClass('active');
-	});
-
-	//---------------------------
-	// setup show hide all button
-	//---------------------------
-	$("#showhide").click(function(){
-		
-		$('.item').toggleClass('active');
-	});
-
-});
-
-console.log('dark mode');
-
-
-$(document).ready(function(){
-
-	//------------------------------------
-	// show hide each itme
-	//------------------------------------
-
-	$('.item').click(function(){
-		$(this).toggleClass('active');
-	});
-
-	//---------------------------
-	// setup show hide all button
-	//---------------------------
-	$("#showhide").click(function(){
-		
-		$('.item').toggleClass('active');
-	});
 
 	//------------------------------------
 	// adds a a darkmode class to the body
 	//------------------------------------
-	$("#darkmode").click(function(){
+	$("#mode").click(function(){
+		console.log('clicked');
 		$('body').toggleClass('darkmode');
 	});
 
