a5d084e408c05b884b9df1e65524db49ac668346
owenxnewman <78100364+owenxnewman@users.noreply.github.com>
2021-05-05 08:01:41 -0700

    push

--- projects/zine/script.js
+++ projects/zine/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
-	console.log('script loaded');
+	console.log('Hi');
+	// let sum = 5 + 5;
+	// console.log(sum);
 
+	$("#mode").click(function(){
+		console.log('clicked');
+		$('body').toggleClass('darkmode');
+	});
+
+
+	$("#apple").click(function(){
+		console.log('clicked');
+		$('h1').toggleClass('special');
+	});
 });
